a131ef2616d00b3917f9493f808296b8c864ed27
repiyann <muhrepiyan26@gmail.com>
2023-12-17 09:41:22 +0700

    Fix NavBar Animation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,21 +18,20 @@
     <!-- Check Responsiveness of a Page -->
 
     <!-- NavBar Start -->
-    <nav class="bg-white shadow-lg sticky">
-        <div class="px-5 pt-3 md:px-28 md:py-5" x-data="{ showMenu: false }">
-            <div class="flex justify-between border-b md:border-none pb-3 md:p-0">
+    <nav class="bg-white shadow-lg sticky top-0">
+        <div class="px-5 pt-3 md:px-28 md:py-5" x-data="{ showMenu: false }" aria-hidden="true">
+            <div class="flex justify-between pb-3 md:p-0" :class="showMenu && 'border-b-2'">
                 <a href="#">
-                    <i class="fa-solid fa-heart-pulse fa-2xl"></i>
+                    <i class="fa-solid fa-van-shuttle fa-2xl"></i>
                 </a>
 
-                <!-- Toggle for hamburger menu in mobile layout -->
-                <div @click="showMenu = true" class="md:hidden">
-                    <button class="group" aria-expanded="false">
-                        <svg class="absolute block transition duration-150" :class="showMenu && 'rotate-45 opacity-0'" xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
+                <!-- Toggle for hamburger menu in mobile layout with animation -->
+                <div @click="showMenu = !showMenu" class="md:hidden">
+                    <button>
+                        <svg class="absolute block transition duration-150" :class="showMenu && 'rotate-45 opacity-0'" xmlns="http://www.w3.org/2000/svg" height="24" width="21" viewBox="0 0 448 512">
                             <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
                         </svg>
                         <svg class="opacity-0 transition duration-150" :class="showMenu && 'rotate-90 opacity-100'" xmlns="http://www.w3.org/2000/svg" height="24" width="18" viewBox="0 0 384 512">
-                            <!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.-->
                             <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
                         </svg>
                     </button>
@@ -40,19 +39,19 @@
 
                 <!-- Middle item navigation for desktop layout -->
                 <div class="hidden md:block">
-                    <a href="#" class="px-3"> Layanan </a>
-                    <a href="#" class="px-3"> About Us </a>
+                    <a href="#" class="px-3 text-lg font-semibold"> Layanan </a>
+                    <a href="#" class="px-3 text-lg font-semibold"> About Us </a>
                 </div>
 
                 <!-- Login button only for desktop layout -->
                 <div class="hidden md:block">
-                    <a href="#" class="px-3"> Daftar </a>
+                    <a href="#" class="px-5 py-2 text-lg font-semibold bg-green-500 rounded-lg hover:bg-green-800 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"> Cek Tiket </a>
                 </div>
             </div>
 
             <!-- Hamburger menu content -->
-            <div x-cloak @click.outside="showMenu = false" x-show="showMenu" x-collapse>
-                <div class="flex justify-center md:hidden pb-3">
+            <div x-cloak x-show="showMenu" @click.outside="showMenu = false">
+                <div class="flex justify-center text-center md:hidden pb-3">
                     <ul>
                         <li class="pt-2">
                             <a href="#"> Layanan </a>
@@ -68,6 +67,11 @@
     </nav>
     <!-- NavBar End -->
 
+    <!-- Hero Start -->
+    <div aria-hidden="true">
+        <h1> Hai </h1>
+    </div>
+    <!-- Hero End -->
 </body>
 
 </html>
