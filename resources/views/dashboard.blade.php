cf12cfe936a35a725b94b178e824ac67e321a89a
repiyann <muhrepiyan26@gmail.com>
2024-02-08 12:05:13 +0700

    refactor(input): refactor input date to have cursor-pointer

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -136,7 +136,7 @@
                             <label for="dateAvailable" class="block text-base font-medium text-[#07074D] dark:text-white">
                                 Date Available
                             </label>
-                            <input type="text" name="dateAvailableTest" id="workDaysInput" class="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
+                            <input type="text" name="dateAvailableTest" readonly id="workDaysInput" class="cursor-pointer w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-4 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md" />
                             <input type="hidden" id="hiddenDateInput" name="dateAvailable">
                         </div>
                         <div class="mb-5">
